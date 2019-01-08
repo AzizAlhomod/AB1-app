793e50a37c8a2806d40b9b965f7596b55738519c
azizalhomod <aalhamoud@gmail.com>
2019-01-08 13:57:47 -0800

    added 3 more table with its calculations and results

--- main2.js
+++ main2.js
@@ -6,7 +6,42 @@ function roundToFifty(someNum){
 
 function GetDose() {
 //initial input by end user
+    // Get all table
     var indication2 = document.getElementById("indication2box").value;
+    var cardiacTable = document.querySelector(".cardiac-wrapper");
+    var dvtTable = document.querySelector(".dvt-wrapper");
+    var strokeTable = document.querySelector(".stroke-wrapper");
+    var procedureTable = document.querySelector(".procedure-wrapper");
+    //Display the right table
+    switch (indication2) {
+        case "Cardiac":
+            cardiacTable.style.display = "block";
+            dvtTable.style.display = "none";
+            strokeTable.style.display = "none";
+            procedureTable.style.display = "none";
+            break;
+        case "DVT/PE":
+            cardiacTable.style.display = "none";
+            dvtTable.style.display = "block";
+            strokeTable.style.display = "none";
+            procedureTable.style.display = "none";
+            break;
+
+        case "Stroke/TIA/Surgery":
+            cardiacTable.style.display = "none";
+            dvtTable.style.display = "none";
+            strokeTable.style.display = "block";
+            procedureTable.style.display = "none";
+            break;
+        case "Transplant/Vascular":
+            cardiacTable.style.display = "none";
+            dvtTable.style.display = "none";
+            strokeTable.style.display = "none";
+            procedureTable.style.display = "block";
+            break;
+
+    }
+//initial input by end user
     var gender2  = document.getElementById("gender2box").value;
     var actualweightlb =  document.getElementById("actualweightlbbox").value;
     var actualweight2 = (actualweightlb * 0.453592);
@@ -42,38 +77,39 @@ function GetDose() {
     var finalBolusRound;
 
 //correction stuff
-    //maximum value for bolus in the adjustment table for DVT is 10000
-dvtboluscormax = 10000;
-boluscor80 = dosingweight2*80;
-boluscor40 = dosingweight2*40;
- if (boluscor80> dvtboluscormax) {boluscor80a = dvtboluscormax} else {boluscor80a =roundToFifty( boluscor80)}
- if (boluscor40> dvtboluscormax) {boluscor40a = dvtboluscormax} else {boluscor40a = roundToFifty (boluscor40)}
+// maximum value for bolus in the adjustment table for DVT is 10000
+    dvtboluscormax = 10000;
+    boluscor80 = dosingweight2*80;
+    boluscor40 = dosingweight2*40;
+    // DVT bolus adjustment values rounded to 50s and compare to max of 10000
+    var boluscor80a;
+    var boluscor40a;
+    if (boluscor80> dvtboluscormax) {boluscor80a = dvtboluscormax} else {boluscor80a =roundToFifty( boluscor80)}
+     if (boluscor40> dvtboluscormax) {boluscor40a = dvtboluscormax} else {boluscor40a = roundToFifty (boluscor40)}
+//cardiac adjustment bolus
+    cardiacbolusmax = 5000;
+    boluscor60 = dosingweight2*60;
+    boluscor30 = dosingweight2*30;
+    // rounded up and compare to max of 5000
+    var boluscor60a;
+    var boluscor30a;
+    if (boluscor60 >cardiacbolusmax) {boluscor60a = cardiacbolusmax} else {boluscor60a = roundToFifty(boluscor60)}
+    if (boluscor30 >cardiacbolusmax) {boluscor30a = cardiacbolusmax} else {boluscor30a = roundToFifty(boluscor30)}
 
-maxadjust = 500;
-calccor60 = dosingweight2*6;
-calccor40 = dosingweight2*4;
-calccor30 = dosingweight2*3;
-calccor20= dosingweight2*2;
-// making sure its less than 500
-    // rounded up
+//infusion adjustment for all
+    maxadjust = 500;
+    infusioncor4 = dosingweight2*4;
+    infusioncor3 = dosingweight2*3;
+    infusioncor2= dosingweight2*2;
+    // rounded up and compare to max of 500
+    var infusioncor4a;
+    var infusioncor3a;
+    var infusioncor2a;
+    if (infusioncor4 >maxadjust) {infusioncor4a = maxadjust} else {infusioncor4a = roundToFifty(infusioncor4)}
+    if (infusioncor3 >maxadjust) {infusioncor3a = maxadjust} else {infusioncor3a = roundToFifty(infusioncor3)}
+    if (infusioncor2 >maxadjust) {infusioncor2a = maxadjust} else {infusioncor2a = roundToFifty(infusioncor2)}
 
-    if (calccor60 >maxadjust) {cor60 = maxadjust} else {cor60 = roundToFifty(calccor60)}
-    if (calccor40 >maxadjust) {cor40 = maxadjust} else {cor40 = roundToFifty(calccor40)}
-    if (calccor30 >maxadjust) {cor30 = maxadjust} else {cor30 = roundToFifty(calccor30)}
-    if (calccor20 >maxadjust) {cor20 = maxadjust} else {cor20 = roundToFifty(calccor20)}
-
-
-
-
-
-
-
-
-
-
-
-
-//indication based calculation
+    //indication based calculation
 
 
     if (indication2 == "Cardiac") {
@@ -98,9 +134,9 @@ calccor20= dosingweight2*2;
     }
     else if (indication2 == "DVT/PE") {
         protocol2 = "DVT/PE";
-        bolus2 = 0;
-        infusion2 = 12;
-        maxbolus2 = 0;
+        bolus2 = 80;
+        infusion2 = 18;
+        maxbolus2 = 10000;
         calculatedbolus2 = bolus2 * dosingweight2;
         calculatedinfusion2 = infusion2 * dosingweight2;
         maxinfusion2 = 1000;
@@ -118,12 +154,13 @@ calccor20= dosingweight2*2;
     }
     else if (indication2 == "Stroke/TIA/Surgery") {
         protocol2 = "Stroke/TIA/Surgery";
-        bolus2 = 80;
-        infusion2 = 18;
+        bolus2 = 0;
+        maxbolus2 = 0;
+        infusion2 = 12;
+        maxinfusion2 = 2100;
         calculatedbolus2 = bolus2 * dosingweight2;
         calculatedinfusion2 = infusion2 * dosingweight2;
-        maxbolus2 = 10000;
-        maxinfusion2 = 2100;
+
         Goal = "72-104";
         if (calculatedbolus2 > maxbolus2) {
             finalbolus2 = maxbolus2
@@ -156,38 +193,50 @@ calccor20= dosingweight2*2;
     //results.push(finalbolus2);
     //results.push(finalinfusion2
     // document.getElementById('res2').value = finalinfusion2;
-    var results2 = { indication: protocol2, weight: actualweight2, height: actualheight2, bolus: finalbolus2, infusion: finalinfusion2, goal: Goal,
-        correction6: cor60, correction4: cor40, correction3 : cor30, correction2: cor20, boluscor80 : boluscor80a, boluscor40 : boluscor40a};
+    var results2 = { indication: protocol2, weight: actualweight2, height: actualheight2, bolus: finalbolus2, infusion: finalinfusion2,
+        goal: Goal,
+        cardiacBolus60: boluscor60a,
+        cardiacBolus30:boluscor30a,
+        dvtBolus80: boluscor80a,
+        dvtBolus40: boluscor40a,
+        infusionX4: infusioncor4a,
+        infusionX3: infusioncor3a,
+        infusionX2: infusioncor2a};
+
+
 
     //alert(JSON.stringify(results2));
 
     document.getElementById('results').innerHTML = JSON.stringify(results2);
     document.getElementById('bolus').innerHTML = results2.bolus;
     document.getElementById('infusion3').innerHTML = results2.infusion;
-    document.getElementById('correct6').innerHTML = results2.correction6;
-    document.getElementById('correct4').innerHTML = results2.correction4;
-    document.getElementById('correct3').innerHTML = results2.correction3;
-    document.getElementById('correct2').innerHTML = results2.correction2;
-    document.getElementById('correct33').innerHTML = results2.correction3;
-    document.getElementById('correct22').innerHTML = results2.correction2;
-    document.getElementById('correct222').innerHTML = results2.correction2;
+    document.getElementById('correct80').innerHTML = results2.dvtBolus80;
+    document.getElementById('correct60').innerHTML = results2.cardiacBolus60;
+    document.getElementById('correct40').innerHTML = results2.dvtBolus40;
+    document.getElementById('correct30').innerHTML = results2.cardiacBolus30;
+    //cardiac infusion adjustment
+    document.getElementById('cardiacRow1').innerHTML = results2.infusionX4;
+    document.getElementById('cardiacRow6').innerHTML = results2.infusionX3;
+    document.getElementById('cardiacRow2').innerHTML = results2.infusionX2;
+    document.getElementById('cardiacRow3').innerHTML = results2.infusionX2;
+    document.getElementById('cardiacRow5').innerHTML = results2.infusionX2;
+    //DVT infusion adjustment
+    document.getElementById('dvtRow1').innerHTML = results2.infusionX4;
+    document.getElementById('dvtRow6').innerHTML = results2.infusionX3;
+    document.getElementById('dvtRow2').innerHTML = results2.infusionX2;
+    document.getElementById('dvtRow3').innerHTML = results2.infusionX2;
+    document.getElementById('dvtRow5').innerHTML = results2.infusionX2;
+    //Stroke infusion adjustment
+    document.getElementById('strokeRow1').innerHTML = results2.infusionX4;
+    document.getElementById('strokeRow6').innerHTML = results2.infusionX4;
+    document.getElementById('strokeRow2').innerHTML = results2.infusionX2;
+    document.getElementById('strokeRow3').innerHTML = results2.infusionX2;
+    document.getElementById('strokeRow5').innerHTML = results2.infusionX3;
+
     document.getElementById('indication').innerHTML = results2.indication;
-    document.getElementById('bolus80').innerHTML= results2.boluscor80;
-    document.getElementById('bolus40').innerHTML= results2.boluscor40;
-    //last column in dvt table
-    document.getElementById('dvtrow1').innerHTML = results2.correction4;
-    document.getElementById('dvtrow2').innerHTML = results2.correction2;
-    document.getElementById('dvtrow3').innerHTML = results2.correction2;
 
-    document.getElementById('dvtrow5').innerHTML = results2.correction2;
-    document.getElementById('dvtrow6').innerHTML = results2.correction3;
-//last column in stroke table
-    document.getElementById('strokerow1').innerHTML = results2.correction4;
-    document.getElementById('strokerow2').innerHTML = results2.correction2;
-    document.getElementById('strokerow3').innerHTML = results2.correction2;
 
-    document.getElementById('strokerow5').innerHTML = results2.correction3;
-    document.getElementById('strokerow6').innerHTML = results2.correction4;
+
 
 
 
